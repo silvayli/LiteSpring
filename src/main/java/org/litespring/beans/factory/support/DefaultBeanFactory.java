9aebd560d7047af9ec9e54b514f8acf96a865c7c
Alvin <conf1102@163.com>
2018-07-13 00:47:24 +0800

    Test Git

---
+++ src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -0,0 +1,19 @@
+package org.litespring.beans.factory.support;
+
+import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.factory.BeanFactory;
+
+public class DefaultBeanFactory implements BeanFactory {
+
+    public BeanDefinition getBeanDefinition(String beanID) {
+        return null;
+    }
+
+    public DefaultBeanFactory(String configFile) {
+
+    }
+
+    public Object getBean(String beanID) {
+        return null;
+    }
+}
